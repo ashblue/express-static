9c5051d67c4e24bbf01a0d6776cf24edacb9705c
Ash Blue <ash@blueashes.com>
2015-12-18 13:45:22 -0700

    Made the application friendly to JS frameworks out of the box

--- app.js
+++ app.js
@@ -7,16 +7,21 @@ var bodyParser = require('body-parser');
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+// We are going to force redirect the user always to our root
+// This is required for frameworks like Ember and Angular
+app.get('*', function(req, res) {
+  res.sendfile('./public/index.html');
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
